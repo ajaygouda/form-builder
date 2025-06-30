02b3e7b6a232e5b2b8b9291a9a39a9c18736e1e2
Ajay Gouda <ajaygouda10@gmail.com>
2025-06-30 18:50:39 +0530

    ignoe eslint errors

--- src/app/campaigns/page.tsx
+++ src/app/campaigns/page.tsx
@@ -1,5 +1,6 @@
 import { Navbar } from "@/components/navbar"
 import { ICampaign } from "@/types/ICampaign"
+import Link from "next/link"
 
 const campaigns: ICampaign[] = [
     {
@@ -39,14 +40,14 @@ const page = () => {
         <>
             <Navbar name="Campaigns" isSearch={true} />
             <div className="grid grid-cols-2 md:grid-cols-4 gap-[20px]">
-                <a href="/campaigns/0" className="flex h-48 items-center justify-center p-6 bg-white border border-gray-200 rounded-sm dark:bg-gray-800 dark:border-gray-700 block p-6 bg-white border border-gray-200 rounded-lg hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+                <Link href="/campaigns/0" className="flex h-48 items-center justify-center p-6 bg-white border border-gray-200 rounded-sm dark:bg-gray-800 dark:border-gray-700 block p-6 bg-white border border-gray-200 rounded-lg hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
                     <div className="flex flex-col items-center justify-center ">
                         <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#1f1f1f"><path d="M440-120v-320H120v-80h320v-320h80v320h320v80H520v320h-80Z" /></svg>
                         <p className="font-normal line-clamp-2 text-sm text-gray-500 dark:text-gray-400 mt-3">New Campaign</p>
                     </div>
-                </a>
+                </Link>
                 {campaigns?.map((item: ICampaign, index: number) => (
-                    <a key={index} href={`/campaigns/${item.id}`} className="p-6 bg-white border border-gray-200 rounded-sm dark:bg-gray-800 dark:border-gray-700 block p-6 bg-white border border-gray-200 rounded-lg hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+                    <Link key={index} href={`/campaigns/${item.id}`} className="p-6 bg-white border border-gray-200 rounded-sm dark:bg-gray-800 dark:border-gray-700 block p-6 bg-white border border-gray-200 rounded-lg hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
                         <h5 className="mb-2 text-xl line-clamp-1 font-medium tracking-tight text-gray-900 dark:text-white">{item.title}</h5>
                         <p className="font-normal line-clamp-2 text-sm text-gray-500 dark:text-gray-400">{item.description}</p>
                         <div className="flex justify-between items-center mt-10">
@@ -62,7 +63,7 @@ const page = () => {
 
                             <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#1f1f1f"><path d="M647-440H160v-80h487L423-744l57-56 320 320-320 320-57-56 224-224Z" /></svg>
                         </div>
-                    </a>
+                    </Link>
                 ))}
             </div>
         </>
