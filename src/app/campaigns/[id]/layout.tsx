b0ebb8b32723c63033ee6242a8d7cf922c409a5a
Ajay Gouda <ajaygouda10@gmail.com>
2025-06-30 19:17:59 +0530

    Fixes-4

--- src/app/campaigns/[id]/layout.tsx
+++ src/app/campaigns/[id]/layout.tsx
@@ -1,21 +1,23 @@
 "use client"
 import { Navbar } from './components/navbar';
-import { use, useState } from 'react';
+import { useState } from 'react';
 import { useRouter } from "next/navigation";
 
-export default function CampaignTabLayout({ children, params }: {
+export default function CampaignTabLayout({
+    children,
+    params,
+}: {
     children: React.ReactNode;
-    params: Promise<{ id: string }>;
+    params: { id: string };
 }) {
+    const { id } = params;
+    const router = useRouter();
+    const [activeTab, setActiveTab] = useState<number>(1);
 
-    const { id } = use(params); 
-    const router:any = useRouter();
-    const [activeTab, setActiveTab] = useState<any>(1);
-
-    const handleTab = (tab: any) => {
-        setActiveTab(tab.id)
+    const handleTab = (tab: { id: number; label: string }) => {
+        setActiveTab(tab.id);
         router.push(`/campaigns/${id}/${tab.label.toLowerCase()}`);
-    }
+    };
 
     return (
         <>
