452720553e4dc1bb2cad2ce88e6627c8567138f9
Ajay Gouda <ajaygouda10@gmail.com>
2025-06-30 18:52:54 +0530

    Fixes

--- src/app/campaigns/[id]/page.tsx
+++ src/app/campaigns/[id]/page.tsx
@@ -1,5 +1,5 @@
 import { redirect } from 'next/navigation';
 
-export default function Page({ params }: { params: { id: string } }) {
-  redirect(`/campaigns/${params.id}/form`);
+export default function Page(props:any) {
+  redirect(`/campaigns/${props.id}/form`);
 }
